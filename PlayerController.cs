76f9df150a7b7a3737d8c754b6a27f7b7375c0af
Perry Houser <pihouser5@gmail.com>
2017-02-22 10:08:08 -0700

    Add files via upload
    Scripts from Unity 5.51f1

---
+++ PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+    private int count;
+
+    public Text countText;
+    public Text winText;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent< Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+        private void FixedUpdate()
+    {
+        //Physics Code here
+        float moveHorizontal = Input.GetAxis("Horizontal"); // maps the keyboard movement to player
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+        }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 13)
+        {
+            winText.text = "You Win!";
+        }
+    }
+         
+}
---
+++ Rotater.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour {
+	
+	// Update is called once per frame, no forces so we can use update not lateupdate
+	void Update () {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime); //smoothes rotate
+	}
+}
